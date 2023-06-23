8b236055cbfe620f7196b4e459e89cf0f23b0a37
clakr <clarktolosa@gmail.com>
2023-06-23 10:43:39 +0800

    mobile, desktop done, to deploy

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
-import "./style.css";
+import "./style.scss";
 
 function changeTextContent(element: HTMLTitleElement | Element | null) {
   if (!element) return;
 
   element.textContent = import.meta.env.VITE_PROJECT_NAME;
+}
+
+function setInputState(formData: FormData, inputName: string) {
+  const input = document.querySelector(`input[name=${inputName}]`);
+  if (!input) return;
+
+  if (!formData.get(inputName)) {
+    input.classList.add("form__input--invalid");
+    return;
+  }
+
+  input.classList.remove("form__input--invalid");
 }
 
 (function () {
@@ -12,4 +24,19 @@ function changeTextContent(element: HTMLTitleElement | Element | null) {
 
   const heading = document.querySelector(".sr-only");
   changeTextContent(heading);
+
+  const form = document.querySelector("form");
+  if (!form) return;
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const inputNames = ["firstName", "lastName", "email", "password"];
+
+    const formData = new FormData(this);
+
+    for (const inputName of inputNames) {
+      setInputState(formData, inputName);
+    }
+  });
 })();
